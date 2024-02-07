03819ea34bb8ee05f250b4d8aa7e3c8d0effbc31
pauloJ-r <pj74586@gmail.com>
2024-02-07 20:31:28 -0300

    adicionando header inicial

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,7 +1,10 @@
 import Image from "next/image";
+import Header from "../_components/hearder";
 
 export default function Home() {
   return (
-   <h1>Aboout</h1>
+   <div>
+    <Header/>
+   </div>
   );
 }
