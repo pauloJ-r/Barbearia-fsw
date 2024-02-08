189d2ff5a3afc2e2017ece879de9e7459ef1fde5
pauloJ-r <pj74586@gmail.com>
2024-02-07 21:33:42 -0300

    feat: adicionando componente booking

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import Header from "../_components/hearder";
 import { de, ptBR } from "date-fns/locale";
 import Search from "./_componentes/search";
+import BookingItem from "../_components/ui/booking-item";
 
 export default function Home() {
   return (
@@ -19,6 +20,10 @@ export default function Home() {
     </div>
     <div className="px-5 mt-6">
     <Search/>
+    </div>
+    <div className="px-5 mt-6">
+      <h2 className="text-sm uppercase text-gray-400 font-bold mb-3">Agendamentos</h2>
+     <BookingItem/>
     </div>
    </div>
   );
