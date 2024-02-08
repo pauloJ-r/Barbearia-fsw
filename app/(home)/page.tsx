e11546e7bbdc3ea23c94d10e5a9cf5951e1067b1
pauloJ-r <pj74586@gmail.com>
2024-02-07 22:49:25 -0300

    feat: adicionando babershop versão inicial

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -4,8 +4,13 @@ import Header from "../_components/hearder";
 import { de, ptBR } from "date-fns/locale";
 import Search from "./_componentes/search";
 import BookingItem from "../_components/ui/booking-item";
+import BarbershopItem from "./_componentes/barbershop-item";
+import { db } from "../_lib/prisma";
 
-export default function Home() {
+export default async function Home() {
+
+  const barbershops = await db.barbershop.findMany({})
+
   return (
    <div>
     <Header/>
@@ -24,6 +29,17 @@ export default function Home() {
     <div className="px-5 mt-6">
       <h2 className="text-sm uppercase text-gray-400 font-bold mb-3">Agendamentos</h2>
      <BookingItem/>
+    </div>
+    <div className="mt-6">
+    <h2 className=" px-5 text-sm uppercase text-gray-400 font-bold mb-3">Recomendados</h2>
+    <div className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+          {barbershops.map((barbershop) => (
+            <div key={barbershop.id} className="min-w-[167px] max-w-[167px]">
+              <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+            </div>
+          ))}
+        </div>
+
     </div>
    </div>
   );
